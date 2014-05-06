b3921ad61c7555b0d94ae3cb1e9b856e80aadeed
Marc <marc.kassubeck@googlemail.com>
2014-05-06 18:08:31 +0200

    Anfänge von Aufgabe 2

--- ex01/task2.c
+++ ex01/task2.c
@@ -1,3 +1,4 @@
+#include <sys/time.h> 
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,29 +16,44 @@ int s;
 //Function to get the current time in seconds
 double get_time()
 {
-  struct timeval time;
-  gettimeofday(&time, NULL);
-  return (double) time.tv_sec + (time.tv_usec/1000000.0);
+	struct timeval time;
+	gettimeofday(&time, NULL);
+	return (double) time.tv_sec + (time.tv_usec/1000000.0);
 }
 
-int main(void) {
-  // Array of length n with random numbers
-  n = 10;
-  double* x = malloc(n * sizeof(double));
-  // TO-DO Init with random numbers
-  for (i = 0; i < n; i++) {
-    x[i] = i;
-  }
-  // Calculate sum
-  s = 1;
-  start_time = get_time();
-  for (j =0; j < n; j+=s) {
-    sum +=x[j];
-  }
-  // Stop time, print sum and time
-  free(x);
-  stop_time = get_time();
-  end_time = stop_time - start_time;
-  printf("Sum: %f \n", sum);
-  printf("Computation time: %fs", end_time);
+void calcSum(int n, int s)
+{
+	// Array of length n with random numbers
+	double* x = (double*)malloc(n * sizeof(double));
+	// TO-DO Init with random numbers
+	for (i = 0; i < n; i++) {
+		x[i] = rand();
+	}
+
+	// Calculate sum
+	start_time = get_time();
+	for (j =0; j < n; j+=s) {
+		sum +=x[j];
+	}
+	// Stop time, print sum and time
+	free(x);
+	stop_time = get_time();
+	end_time = stop_time - start_time;
+	printf("Sum: %f \n", sum);
+	printf("Computation time: %fs \n", end_time);
+	printf("n:%i s:%i \n \n", n , s);
 }
+
+
+int main(void) 
+{
+	for(int s = 1 ; s <= 10; s++)
+	{
+		for(int n=1000 ; n <= 10000000 ; n+=1000)
+		{	
+			calcSum(n,s);
+		}
+	}
+}
+
+
--- ex01/task3.c
+++ ex01/task3.c
@@ -6,48 +6,48 @@
 
 double get_time()
 {
-  struct timeval time;
-  gettimeofday(&time, NULL);
-  return (double) time.tv_sec + (time.tv_usec/1000000.0);
+	struct timeval time;
+	gettimeofday(&time, NULL);
+	return (double) time.tv_sec + (time.tv_usec/1000000.0);
 }
 
 
 int main(int argc, char **argv) {
-  MPI_Init(&argc, &argv);
+	MPI_Init(&argc, &argv);
 
-  int whoAmI;
-  MPI_Comm_rank(MPI_COMM_WORLD, &whoAmI);
-  printf("I am %d\n", whoAmI);
+	int whoAmI;
+	MPI_Comm_rank(MPI_COMM_WORLD, &whoAmI);
+	printf("I am %d\n", whoAmI);
 
-  MPI_Status stat;
+	MPI_Status stat;
 
-  double start_time;
-  double end_time;
-  float runtime;
-  int size = 1000;
-  switch (whoAmI) {
-  case 0: {
-      char* x = malloc(size * 1024 * sizeof(char));
-      start_time = get_time();
-      MPI_Send(x, size * 1024, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-      MPI_Recv(x, 0, MPI_CHAR, 1, 0, MPI_COMM_WORLD, &stat);
-      end_time = get_time();
-      runtime = end_time - start_time; 
-      printf("%i kb,%f s,%f kbs\n", size, runtime, (size/runtime));
-      free(x);
-      break;
-  }
-  case 1: {
-    char* x = malloc(size * 1024 * sizeof(char));
-    MPI_Recv(x, size*1024, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &stat);
-    MPI_Send(x, 0, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-    free(x);
-    break;
-  }
-  default:
-    printf("hello. i am your missing child! #%d\n", whoAmI);
-  }
+	double start_time;
+	double end_time;
+	float runtime;
+	int size = 1000;
+	switch (whoAmI) {
+	case 0: {
+		char* x = malloc(size * 1024 * sizeof(char));
+		start_time = get_time();
+		MPI_Send(x, size * 1024, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+		MPI_Recv(x, 0, MPI_CHAR, 1, 0, MPI_COMM_WORLD, &stat);
+		end_time = get_time();
+		runtime = end_time - start_time; 
+		printf("%i kb,%f s,%f kbs\n", size, runtime, (size/runtime));
+		free(x);
+		break;
+			}
+	case 1: {
+		char* x = malloc(size * 1024 * sizeof(char));
+		MPI_Recv(x, size*1024, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &stat);
+		MPI_Send(x, 0, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+		free(x);
+		break;
+			}
+	default:
+		printf("hello. i am your missing child! #%d\n", whoAmI);
+	}
 
-  MPI_Finalize();
-  return 0;
+	MPI_Finalize();
+	return 0;
 }
