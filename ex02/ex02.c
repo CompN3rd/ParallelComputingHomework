8643eb39bad29f99e5e8a089fa2e442f8b82aff1
Torsten Thoben <t.thoben@freenet.de>
2014-05-13 09:16:24 +0200

    fixed use of bit operators; clean up source code

--- ex02/ex02.c
+++ ex02/ex02.c
@@ -12,9 +12,12 @@ int main(int argc, char** artgv)
 	MPI_Status stat;
 
 	MPI_Init(NULL, NULL);
-
-	char* x = (char*)malloc(1024 * sizeof(char));
-    
+    /*	MPI							C
+		MPI_CHAR 			-> signed char
+		MPI_UNSIGNED_CHAR	-> unsigned char
+		From slides 2 page 32
+		I think if we use MPI_CHAR we should use signed char in c
+	*/
 	signed char* response = (signed char*)malloc(sizeof(signed char));
 	signed int* eatReq = (int*)malloc( sizeof(signed int));
 	
@@ -23,17 +26,15 @@ int main(int argc, char** artgv)
 
 	if(whoAmI == 0)
 	{
-		char byte = 0;
-		// printf("Hello world from processor %s (pid %d), rank %d out of %d!\n", proc_name, getpid(), whoAmI, worldSize);
-		// was ist wenn 0 als erste ist 10sec vergehen und kein Recv kommt ?
-		// macht doch keinen Sinn 
+		char 	byte = 0;
+		int		left = 0, 
+				right= 0;
+		
 		while(counter < worldSize)
 		{
-
 			MPI_Recv(eatReq, sizeof(signed int), MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &stat);
 			
-			int left,right;
-			
+			// set neighbours
 			if(eatReq[0] == 1)
 			{
 				left = 5;
@@ -48,43 +49,48 @@ int main(int argc, char** artgv)
 				right = eatReq[0] + 1;
 			}
 			
-			// Bit Abfrage ob rechts oder links die Gabel weg ist
-			if( byte & left || byte & right)
+			// Bit wise: is fork available?
+			if( byte & (1 << left) || byte & ( 1 << right ))
 			{
-				// nicht essen -> sende warten
+				// forks are in use -> send 'W' for wait
 				response[0] = 'W';
 				MPI_Send(response, sizeof(signed char), MPI_CHAR, eatReq[0], 1, MPI_COMM_WORLD);
 			}else
 			{
-				// essen -> sende Iss
-				response[0] = 'I';
+				// forks not in use -> send 'E' for Eat! 
+				response[0] = 'E';
 				MPI_Send(response, sizeof(signed char), MPI_CHAR, eatReq[0], 1, MPI_COMM_WORLD);
-				byte ^ eatReq[0]; // Bit setzen => er isst
+				byte |= (1 << eatReq[0]); // set bit at pos eatReq[0]
+				//  byte or ...
 			}
 			
+			// ToDo:
+			/*
+				Receive that the Process has finish eating
+				byte &= ~(1 << finish);   delete bit for this process
+	
+			*/		
 			counter++;
 		}
 	}
 	else
-	{
+	{	
+		eatReq[0]		= whoAmI;	
+		char hungrig	= 1;
 		
-		//sprintf(x, "Hello world from processor %s (pid %d), rank %d out of %d!\n", proc_name, getpid(), whoAmI, worldSize);
-		eatReq[0] = whoAmI;
-		
-		
-		char hungrig = 1;
 		while(hungrig)
 		{
 			MPI_Send(eatReq, sizeof(signed int), MPI_INT, 0, 0, MPI_COMM_WORLD);
-		
-		
 			MPI_Recv(response, sizeof(signed char), MPI_CHAR, 0, 1, MPI_COMM_WORLD, &stat);
 		
 			if(response[0] == 'W')
 			{
 				// std::cout << "Ich darf nichts essen" << std::endl;
 				printf("Ich darf nichts essen");
-			}else if(response[0] == 'I')
+				/* ToDo:
+					Wartezeit ? 
+				*/
+			}else if(response[0] == 'E')
 			{
 				// std::cout << "Endlich darf ich essen" << std::endl;
 				printf("Endlich darf ich essen... Jam Jam Jam");
@@ -93,10 +99,8 @@ int main(int argc, char** artgv)
 		}
 	}
 	
-	free(x);
 	free(eatReq);
 	free(response);
-	
 	
 	MPI_Finalize();
 }
