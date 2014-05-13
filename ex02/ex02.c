ccd8c5aca7f0e3d6a7e90f2af2c78160f46bbce4
Marc <marc.kassubeck@googlemail.com>
2014-05-13 23:10:57 +0200

    More cleanup. Implemented putting down forks. More randomness in "thinking" and "eating" times. More output (but I think we need time stamps when which philosopher took a fork and when he put it back down).

--- ex02/ex02.c
+++ ex02/ex02.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 int main(int argc, char** artgv)
 {
 	int resultLen;
 	int whoAmI;
 	int worldSize;
-	int counter = 1;
+	int counter = 0;
 	MPI_Status stat;
 
 	MPI_Init(NULL, NULL);
@@ -18,89 +20,121 @@ int main(int argc, char** artgv)
 		From slides 2 page 32
 		I think if we use MPI_CHAR we should use signed char in c
 	*/
-	signed char* response = (signed char*)malloc(sizeof(signed char));
-	signed int* eatReq = (int*)malloc( sizeof(signed int));
+	signed char response;
+	signed int upDownReq;
+
+	//seed random numbers
+	srand(time(NULL));
 	
 	MPI_Comm_rank(MPI_COMM_WORLD, &whoAmI);
 	MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
 
 	if(whoAmI == 0)
 	{
-		char 	byte = 0;
+		char 	usedForks = 0;
 		int		left = 0, 
 				right= 0;
 		
-		while(counter < worldSize)
+		while(counter < 100)
 		{
-			MPI_Recv(eatReq, sizeof(signed int), MPI_CHAR, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &stat);
+			MPI_Recv(&upDownReq, sizeof(signed int), MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
 			
 			// set neighbours
-			if(eatReq[0] == 1)
+			if(upDownReq == 1)
 			{
 				left = 5;
 				right = 2;
-			}else if(eatReq[0] == 5)
+			}else if(upDownReq == 5)
 			{
 				left = 4;
 				right = 1;
 			}else
 			{
-				left = eatReq[0] - 1;
-				right = eatReq[0] + 1;
+				left = upDownReq - 1;
+				right = upDownReq + 1;
 			}
 			
-			// Bit wise: is fork available?
-			if( byte & (1 << left) || byte & ( 1 << right ))
+			//check if forks are put up or put down
+			if (stat.MPI_TAG == 0)
 			{
-				// forks are in use -> send 'W' for wait
-				response[0] = 'W';
-				MPI_Send(response, sizeof(signed char), MPI_CHAR, eatReq[0], 1, MPI_COMM_WORLD);
-			}else
-			{
-				// forks not in use -> send 'E' for Eat! 
-				response[0] = 'E';
-				MPI_Send(response, sizeof(signed char), MPI_CHAR, eatReq[0], 1, MPI_COMM_WORLD);
-				byte |= (1 << eatReq[0]); // set bit at pos eatReq[0]
-				//  byte or ...
+				// Bit wise: is fork available?
+				if (usedForks & (1 << left) || usedForks & (1 << right))
+				{
+					// forks are in use -> send 'W' for wait
+					response = 'W';
+					MPI_Send(&response, sizeof(signed char), MPI_CHAR, upDownReq, 1, MPI_COMM_WORLD);
+				}
+				else
+				{
+					// forks not in use -> send 'E' for Eat! 
+					response = 'E';
+					MPI_Send(&response, sizeof(signed char), MPI_CHAR, upDownReq, 1, MPI_COMM_WORLD);
+					usedForks |= (1 << upDownReq); // set bit at pos upDownReq
+				}
 			}
-			
-			// ToDo:
-			/*
-				Receive that the Process has finish eating
-				byte &= ~(1 << finish);   delete bit for this process
-	
-			*/		
+			else if (stat.MPI_TAG == 1)
+			{
+				//are both forks really held by this philosopher
+				if (usedForks & (1 << left) && usedForks & (1 << right))
+				{
+					usedForks &= ~(1 << upDownReq); //delete bit for this process
+				}
+				else
+				{
+					printf("Du (%d) hattest doch gar nicht beide Gabeln!", upDownReq);
+				}
+			}
+			else
+			{
+				printf("Ungültige Kellneranfrage!");
+				break;
+			}
+
 			counter++;
 		}
+		printf("Kellner geht nach Hause");
 	}
 	else
-	{	
-		eatReq[0]		= whoAmI;	
-		char hungrig	= 1;
-		
-		while(hungrig)
+	{
+		upDownReq = whoAmI;
+		//each Process has to eat 10 times
+		while (counter < 10)
 		{
-			MPI_Send(eatReq, sizeof(signed int), MPI_INT, 0, 0, MPI_COMM_WORLD);
-			MPI_Recv(response, sizeof(signed char), MPI_CHAR, 0, 1, MPI_COMM_WORLD, &stat);
-		
-			if(response[0] == 'W')
+			char hungry = rand() % 2;
+			while (hungry != 1)
 			{
-				// std::cout << "Ich darf nichts essen" << std::endl;
-				printf("Ich darf nichts essen");
-				/* ToDo:
-					Wartezeit ? 
-				*/
-			}else if(response[0] == 'E')
+				//do intense thinking
+				sleep(rand() % 100);
+				hungry = rand() % 2;
+			}
+
+			while (hungry)
 			{
-				// std::cout << "Endlich darf ich essen" << std::endl;
-				printf("Endlich darf ich essen... Jam Jam Jam");
-				hungrig = 0;
+				MPI_Send(&upDownReq, sizeof(signed int), MPI_INT, 0, 0, MPI_COMM_WORLD);
+				MPI_Recv(&response, sizeof(signed char), MPI_CHAR, 0, 1, MPI_COMM_WORLD, &stat);
+
+				if (response == 'W')
+				{
+					printf("Ich darf nichts essen (%d)", whoAmI);
+
+					// wait a little time (do some thinking)
+					sleep(rand() % 100);
+				}
+				else if (response == 'E')
+				{
+					printf("Endlich darf ich (%d) essen... Jam Jam Jam", whoAmI);
+					// eat a little time
+					sleep(rand() % 100);
+
+					MPI_Send(&upDownReq, sizeof(signed int), MPI_INT, 0, 1, MPI_COMM_WORLD);
+
+					hungry = 0;
+					counter++;
+				}
 			}
 		}
+		printf("Philosoph (%d) meldet sich ab!", whoAmI);
 	}
-	
-	free(eatReq);
-	free(response);
-	
+
 	MPI_Finalize();
 }
