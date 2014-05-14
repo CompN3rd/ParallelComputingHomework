cbc0db7709e4de94b837972a482d058e9cd46b8f
Marc <marc.kassubeck@googlemail.com>
2014-05-14 23:38:15 +0200

    Exercise 2 finished (hopefully)

--- ex02/ex02.c
+++ ex02/ex02.c
@@ -1,43 +1,43 @@
-#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#ifndef WIN32
 #include <unistd.h>
+#endif
 
-int main(int argc, char** artgv)
+#include <mpi.h>
+
+int main(int argc, char* argv[])
 {
-	int resultLen;
 	int whoAmI;
 	int worldSize;
+
+	signed char response;
+	signed char upDownReq;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &whoAmI);
+	MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+
 	int counter = 0;
 	MPI_Status stat;
-
-	MPI_Init(NULL, NULL);
-    /*	MPI							C
-		MPI_CHAR 			-> signed char
-		MPI_UNSIGNED_CHAR	-> unsigned char
-		From slides 2 page 32
-		I think if we use MPI_CHAR we should use signed char in c
-	*/
-	signed char response;
-	signed int upDownReq;
 
 	//seed random numbers
 	srand(time(NULL));
 	
-	MPI_Comm_rank(MPI_COMM_WORLD, &whoAmI);
-	MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
-
 	if(whoAmI == 0)
 	{
 		char 	usedForks = 0;
 		int		left = 0, 
 				right= 0;
+
+		printf("Kellner (%d) meldet sich an!\n", whoAmI);
+		fflush(stdout);
 		
-		while(counter < 100)
+		while(counter < worldSize - 1)
 		{
-			MPI_Recv(&upDownReq, sizeof(signed int), MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
+			MPI_Recv(&upDownReq, sizeof(signed char), MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
 			
 			// set neighbours
 			if(upDownReq == 1)
@@ -75,28 +75,27 @@ int main(int argc, char** artgv)
 			else if (stat.MPI_TAG == 1)
 			{
 				//are both forks really held by this philosopher
-				if (usedForks & (1 << left) && usedForks & (1 << right))
-				{
-					usedForks &= ~(1 << upDownReq); //delete bit for this process
-				}
-				else
-				{
-					printf("Du (%d) hattest doch gar nicht beide Gabeln!", upDownReq);
-				}
+				usedForks &= ~(1 << upDownReq); //delete bit for this process
+			}
+			else if (stat.MPI_TAG == 2)
+			{
+				//This philosopher is finished
+				counter++;
 			}
 			else
 			{
-				printf("Ungültige Kellneranfrage!");
+				printf("Ungültige Kellneranfrage!\n");
 				break;
 			}
-
-			counter++;
 		}
-		printf("Kellner geht nach Hause");
+		printf("Kellner geht nach Hause\n");
+		fflush(stdout);
 	}
 	else
 	{
 		upDownReq = whoAmI;
+		printf("Philosoph %d geht an den Tisch\n", whoAmI);
+		fflush(stdout);
 		//each Process has to eat 10 times
 		while (counter < 10)
 		{
@@ -104,37 +103,58 @@ int main(int argc, char** artgv)
 			while (hungry != 1)
 			{
 				//do intense thinking
-				sleep(rand() % 100);
+#ifndef WIN32
+				sleep(rand() % 2);
+#else
+				_sleep(rand() % 2);
+#endif
 				hungry = rand() % 2;
 			}
 
 			while (hungry)
 			{
-				MPI_Send(&upDownReq, sizeof(signed int), MPI_INT, 0, 0, MPI_COMM_WORLD);
+				MPI_Send(&upDownReq, sizeof(signed char), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 				MPI_Recv(&response, sizeof(signed char), MPI_CHAR, 0, 1, MPI_COMM_WORLD, &stat);
 
 				if (response == 'W')
 				{
-					printf("Ich darf nichts essen (%d)", whoAmI);
+					printf("Ich darf nichts essen (%d)\n", whoAmI);
+					fflush(stdout);
 
 					// wait a little time (do some thinking)
-					sleep(rand() % 100);
+#ifndef WIN32
+					sleep(rand() % 2);
+#else
+					_sleep(rand() % 2);
+#endif
 				}
 				else if (response == 'E')
 				{
-					printf("Endlich darf ich (%d) essen... Jam Jam Jam", whoAmI);
+					printf("Endlich darf ich (%d) essen... Jam Jam Jam\n", whoAmI);
+					fflush(stdout);
 					// eat a little time
-					sleep(rand() % 100);
+#ifndef WIN32
+					sleep(rand() % 2);
+#else
+					_sleep(rand() % 2);
+#endif
 
-					MPI_Send(&upDownReq, sizeof(signed int), MPI_INT, 0, 1, MPI_COMM_WORLD);
+					//Put down fork
+					MPI_Send(&upDownReq, sizeof(signed char), MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+					printf("Ich (%d) denke jetzt lieber noch ein wenig nach\n", whoAmI);
+					fflush(stdout);
 
 					hungry = 0;
 					counter++;
 				}
 			}
 		}
-		printf("Philosoph (%d) meldet sich ab!", whoAmI);
+		MPI_Send(&upDownReq, sizeof(signed char), MPI_CHAR, 0, 2, MPI_COMM_WORLD);
+		printf("Philosoph (%d) meldet sich ab!\n", whoAmI);
+		fflush(stdout);
 	}
 
 	MPI_Finalize();
+
+	return 0;
 }
